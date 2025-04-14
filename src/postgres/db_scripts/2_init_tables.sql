f96197906d5ddfd3752b8c27926ed2e036a45f54
Zabelin Nikita <n.zabelin@nicct.ru>
2025-04-14 23:30:42 +0300

    Add indexes

--- src/postgres/db_scripts/2_init_tables.sql
+++ src/postgres/db_scripts/2_init_tables.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 create table if not exists company.public.account (
     id uuid primary key default uuid_generate_v4(),
-    username text not null,
+    username text not null UNIQUE,
     password text not null,
     first_name text not null,
     second_name text not null,
@@ -34,7 +34,9 @@ create table if not exists company.public.group (
     updated_at timestamptz not null default NOW()
 );
 
-
+-----------------------------------------------------
+-- TODO Move to MongoDB entity
+-----------------------------------------------------
 create table if not exists company.public.cargo (
     id uuid primary key default uuid_generate_v4(),
     title text not null,
@@ -45,6 +47,7 @@ create table if not exists company.public.cargo (
     updated_at timestamptz not null default NOW(),
     constraint fk_creator foreign key (creator_id) references company.public.account(id)
 );
+-----------------------------------------------------
 
 
 create table if not exists company.public.delivery (
@@ -57,11 +60,37 @@ create table if not exists company.public.delivery (
     receiver_id uuid not null,
     cargo_id uuid not null,
 
+    bill_id uuid,
+
+    send_address_id uuid not null,
+    receive_address_id uuid not null,
 
     created_at timestamptz not null default NOW(),
     updated_at timestamptz not null default NOW(),
 
     constraint fk_sender foreign key (sender_id) references company.public.account(id),
     constraint fk_receiver foreign key (receiver_id) references company.public.account(id),
-    constraint fk_cargo foreign key (cargo_id) references company.public.cargo(id)
+
+-----------------------------------------------------
+-- TODO Move to MongoDB entity
+-----------------------------------------------------
+    constraint fk_cargo foreign key (cargo_id) references company.public.cargo(id),
+-----------------------------------------------------
+
+    constraint fk_send_address foreign key (send_address_id) references company.public.address(id),
+    constraint fk_receive_address foreign key (receive_address_id) references company.public.address(id)
+);
+
+
+CREATE TABLE if not exists company.public.address (
+    id uuid primary key default uuid_generate_v4(),
+    country TEXT not null,
+    region TEXT not null,
+    city TEXT not null,
+    street TEXT not null,
+    house TEXT not null,
+    apartment TEXT not null,
+    postal_code TEXT not null
+    created_at timestamptz not null default NOW(),
+    updated_at timestamptz not null default NOW(),
 );
--- src/postgres/db_scripts/3_init_triggers.sql
+++ src/postgres/db_scripts/3_init_triggers.sql
@@ -1,24 +1,34 @@
-CREATE TRIGGER trigger_set_timestamps
+CREATE TRIGGER trigger_set_timestamps_account
 BEFORE INSERT OR UPDATE ON company.public.account
 FOR EACH ROW
 EXECUTE FUNCTION set_timestamps();
 
-CREATE TRIGGER trigger_set_timestamps
+CREATE TRIGGER trigger_set_timestamps_permission
 BEFORE INSERT OR UPDATE ON company.public.permission
 FOR EACH ROW
 EXECUTE FUNCTION set_timestamps();
 
-CREATE TRIGGER trigger_set_timestamps
+CREATE TRIGGER trigger_set_timestamps_group
 BEFORE INSERT OR UPDATE ON company.public.group
 FOR EACH ROW
 EXECUTE FUNCTION set_timestamps();
 
-CREATE TRIGGER trigger_set_timestamps
+
+-----------------------------------------------------
+-- TODO Move to MongoDB entity
+-----------------------------------------------------
+CREATE TRIGGER trigger_set_timestamps_cargo
 BEFORE INSERT OR UPDATE ON company.public.cargo
 FOR EACH ROW
 EXECUTE FUNCTION set_timestamps();
+-----------------------------------------------------
 
-CREATE TRIGGER trigger_set_timestamps
+CREATE TRIGGER trigger_set_timestamps_delivery
 BEFORE INSERT OR UPDATE ON company.public.delivery
+FOR EACH ROW
+EXECUTE FUNCTION set_timestamps();
+
+CREATE TRIGGER trigger_set_timestamps_address
+BEFORE INSERT OR UPDATE ON company.public.address
 FOR EACH ROW
 EXECUTE FUNCTION set_timestamps();
