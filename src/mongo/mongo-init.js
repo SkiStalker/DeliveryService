e160dcc5905879489a790771a78fba14bc6e8904
Zabelin Nikita <asdfghjkliop44@gmail.com>
2025-08-07 14:07:42 +0300

    Remove russian comments from mongo init scripts

--- src/mongo/mongo-init.js
+++ src/mongo/mongo-init.js
@@ -1,10 +1,8 @@
 db = db.getSiblingDB('company');
 
-// Создаем коллекцию и вставляем заглушку
 db.payments.insertOne({
     _id: "init-temp-doc",
     temp: true
 });
 
-// Удаляем заглушку
 db.payments.deleteOne({ _id: "init-temp-doc" });
